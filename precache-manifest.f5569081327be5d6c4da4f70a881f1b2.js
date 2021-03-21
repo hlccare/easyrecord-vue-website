b145c46585510ecd818114b7109ba24a2ae3a2e5
hlccare <280108904@qq.com>
2021-03-21 23:33:39 +0800

    deploy

---
+++ precache-manifest.f5569081327be5d6c4da4f70a881f1b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560fdb516e1b952af09f",
+    "url": "/easyrecord-vue-website/css/app.5ebfbc5d.css"
+  },
+  {
+    "revision": "e4551cdfbb0e762bf20813fa91c238b7",
+    "url": "/easyrecord-vue-website/index.html"
+  },
+  {
+    "revision": "560fdb516e1b952af09f",
+    "url": "/easyrecord-vue-website/js/app.4a064fac.js"
+  },
+  {
+    "revision": "16547b5579428399df7a",
+    "url": "/easyrecord-vue-website/js/chunk-vendors.6c47da82.js"
+  },
+  {
+    "revision": "18b6271305ef9d672a05be8ed1b1c516",
+    "url": "/easyrecord-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/easyrecord-vue-website/robots.txt"
+  }
+]);
